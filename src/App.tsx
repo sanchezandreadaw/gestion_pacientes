f3ab6ecaef4347140cd849dee5b972b61e25fc91
sanchezandreadaw <sanchezmoraandreadaw@gmail.com>
2025-02-25 16:57:10 +0100

    Validacion de campos del formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import PatientForm from "./components/PatientForm"
+import PatientList from "./components/PatientList"
 
 function App() {
 
 
   return (
     <>
-     <h1 className="text-2xl text-blue-600">Gestión de pacientes</h1>
+     <div className="container mx-auto mt-20">
+      <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+        Seguimiento de pacientes {''}
+        <span className="text-indigo-700">Veterinaria</span>
+      </h1>
+     </div>
+
+     <div className="mt-12 md:flex">
+      <PatientForm/>
+      <PatientList/>
+     </div>
     </>
   )
 }
---
+++ src/components/PatientList.tsx
@@ -0,0 +1,5 @@
+export default function PatientList(){
+    return(
+        <div>PatientList</div>
+    )
+}
